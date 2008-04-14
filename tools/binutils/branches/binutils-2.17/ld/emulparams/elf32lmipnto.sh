187d02dd69b2eb4a69ce5b482f407819ba671763
rmansfield <rmansfield@qnx.com>
2008-04-14 14:00:52 +0000

    Create binutils 2.17 branch

---
+++ tools/binutils/branches/binutils-2.17/ld/emulparams/elf32lmipnto.sh
@@ -0,0 +1,7 @@
+# If you change this file, please also look at files which source this one:
+# elf32elmip.sh elf32lsmip.sh
+
+. ${srcdir}/emulparams/elf32bmipnto.sh
+OUTPUT_FORMAT="elf32-qnxlittlemips"
+BIG_OUTPUT_FORMAT="elf32-qnxbigmips"
+LITTLE_OUTPUT_FORMAT="elf32-qnxlittlemips"
