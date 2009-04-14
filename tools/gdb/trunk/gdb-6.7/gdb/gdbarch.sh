71fb2d61b706e009c1827bf19a67567a2cd75747
aristovski@qnx.com <aristovski@qnx.com@402436cc-5737-0410-82d2-8310154eca3c>
2009-04-14 18:24:18 +0000

    PR: 67210 - gdb solib-search-path not set on time when opening core file CI: rmansfield
    PATCH 1.
    
    * doc/observer.texi: Add architecture_changed observer.
    * gdbarch.sh: Generate new observer's notification.

--- tools/gdb/trunk/gdb-6.7/gdb/gdbarch.sh
+++ tools/gdb/trunk/gdb-6.7/gdb/gdbarch.sh
@@ -1147,6 +1147,7 @@ cat <<EOF
 #include "reggroups.h"
 #include "osabi.h"
 #include "gdb_obstack.h"
+#include "observer.h"
 
 /* Static function declarations */
 
@@ -2090,6 +2091,7 @@ deprecated_current_gdbarch_select_hack (struct gdbarch *new_gdbarch)
   gdb_assert (new_gdbarch->initialized_p);
   current_gdbarch = new_gdbarch;
   architecture_changed_event ();
+  observer_notify_architecture_changed (new_gdbarch);
   reinit_frame_cache ();
 }
 
