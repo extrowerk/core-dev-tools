a63d93b429fff056e3e6f73681fd5eb9ee19a749
aristovski@qnx.com <aristovski@qnx.com@402436cc-5737-0410-82d2-8310154eca3c>
2009-04-28 20:24:28 +0000

    PR: 65845 - mudflapth uses dlerror which is not thread safe CI: rmansfield
    * mf-runtime.c (__mf_resolve_single_dynamic): Do not use dlerror to determine success of dlsym.

--- tools/gcc/trunk/libmudflap/mf-runtime.c
+++ tools/gcc/trunk/libmudflap/mf-runtime.c
@@ -627,7 +627,6 @@ __mfu_set_options (const char *optstr)
 void
 __mf_resolve_single_dynamic (struct __mf_dynamic_entry *e)
 {
-  char *err;
 
   assert (e);
   if (e->pointer) return;
@@ -639,16 +638,16 @@ __mf_resolve_single_dynamic (struct __mf_dynamic_entry *e)
 #endif
     e->pointer = dlsym (RTLD_NEXT, e->name);
 
-  err = dlerror ();
-
-  if (err)
-    {
-      fprintf (stderr, "mf: error in dlsym(\"%s\"): %s\n",
-               e->name, err);
-      abort ();
-    }
   if (! e->pointer)
     {
+      const char *err;
+
+      err = dlerror ();
+
+      if (err)
+	fprintf (stderr, "mf: error in dlsym(\"%s\"): %s\n",
+		 e->name, err);
+
       fprintf (stderr, "mf: dlsym(\"%s\") = NULL\n", e->name);
       abort ();
     }
