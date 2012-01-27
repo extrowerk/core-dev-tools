902270a67c63f6d78e995a40658aaa7d09cbdd7e
aristovski <aristovski@qnx.com>
2012-01-27 21:25:59 +0000

    PR:4 CI:aristovski@qnx.com
    Reference ../gdb-prereq/win32/Python27

--- tools/gdb/trunk/gdb-7.3/python-config-cross.sh
+++ tools/gdb/trunk/gdb-7.3/python-config-cross.sh
@@ -7,32 +7,19 @@
 # Gdb configure will pass python-configure.py (path to it) followed by one of
 # the above options.
 
-#python_dir=${PWD}/../../python-2.7/win32-x86-o/installprefix
-python_dir=${PWD}/../../Python27
+gdb_prereq_dir=${PWD}/../../../gdb-prereq/win32
+python_root=${gdb_prereq_dir}/Python27
 python_ver=2.7
 
-#python_root=`echo "${python_dir}/Python${python_ver}" | sed 's,[.],,g'`
-#python_root=${python_dir}/usr
-python_root=${python_dir}
-
 win32_py_include="-I${python_root}/include -I${python_root}/include/python${python_ver} -DWINVER=0x0500 "
-#"-DPy_NO_ENABLE_SHARED"
-
-echo $* >> python.args
 
 if [ $2 == --includes ] ; then
   echo "${win32_py_include}" 
-  echo "Answer: ${win32_py_include}" >>python.args
 elif [ $2 == --ldflags ] ; then
   echo "-L${python_root}/libs  -lpython27 "
-  echo "Answer: -L${python_root}/libs -lpython27 " >>python.args
 elif [ $2 == --exec-prefix ] ; then
   echo "${python_root}"
-  echo "Answer: ${python_root}" >>python.args
 else
   exit 1
 fi
-
-
-
 
