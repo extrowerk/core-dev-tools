2483052d16d9de9b06098c6c9b37f4d407d9bf6d
rmansfield <rmansfield@qnx.com>
2012-02-06 16:30:12 +0000

    PR: 101404 CI: aristovski@qnx.com
    Add python support to darwin builds.

--- tools/gdb/trunk/gdb-7.3/python-config-cross.sh
+++ tools/gdb/trunk/gdb-7.3/python-config-cross.sh
@@ -7,16 +7,31 @@
 # Gdb configure will pass python-configure.py (path to it) followed by one of
 # the above options.
 
-gdb_prereq_dir=${PWD}/../../../gdb-prereq/win32
-python_root=${gdb_prereq_dir}/Python27
-python_ver=2.7
+case ${TARGET_SYSNAME} in
+win32*)
+	gdb_prereq_dir=${PWD}/../../../gdb-prereq/win32
+	python_root=${gdb_prereq_dir}/Python27
+	python_ver=2.7
+	py_include="-I${python_root}/include -I${python_root}/include/python${python_ver} -DWINVER=0x0500 "
+	py_libs="-L${python_root}/libs  -lpython27"
+	;;
+darwin*)
+	python_ver=2.6
+	python_root=/usr/darwin/x86_64-apple-darwin/x86_64-apple-darwin/
+	py_include="-I${python_root}/include -I${python_root}/include/python${python_ver}"
+	py_libs="-lpython2.6"
+	;;
+*)
+	echo "Not configured for python"
+	exit 1
+	;;
+esac
 
-win32_py_include="-I${python_root}/include -I${python_root}/include/python${python_ver} -DWINVER=0x0500 "
 
 if [ $2 == --includes ] ; then
-  echo "${win32_py_include}" 
+  echo "${py_include}" 
 elif [ $2 == --ldflags ] ; then
-  echo "-L${python_root}/libs  -lpython27 "
+  echo "${py_libs}" 
 elif [ $2 == --exec-prefix ] ; then
   echo "${python_root}"
 else
