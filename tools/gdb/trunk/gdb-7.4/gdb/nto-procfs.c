98874659806726af3fdcccf3dd4a9f2593baab35
aristovski <aristovski@qnx.com>
2012-03-07 21:03:01 +0000

    PR:4 CI:aristovski@qnx.com
    Remove reference to nto_init_solib_absolute_prefix for nto host.
    
    Fix build.

--- tools/gdb/trunk/gdb-7.4/gdb/nto-procfs.c
+++ tools/gdb/trunk/gdb-7.4/gdb/nto-procfs.c
@@ -711,7 +711,6 @@ do_attach (ptid_t ptid)
   if (devctl (ctl_fd, DCMD_PROC_STATUS, &status, sizeof (status), 0) == EOK
       && status.flags & _DEBUG_FLAG_STOPPED)
     SignalKill (nto_node (), PIDGET (ptid), 0, SIGCONT, 0, 0);
-  nto_init_solib_absolute_prefix ();
   return ptid_build (PIDGET (ptid), 0, status.tid);
 }
 
