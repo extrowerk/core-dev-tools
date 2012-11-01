c59bb1746117082b1597294cc8195571bd03ceca
aristovski@qnx.com <aristovski@qnx.com@402436cc-5737-0410-82d2-8310154eca3c>
2012-11-01 04:10:07 +0000

    PR: 234296 - gdb-7.5 CI:aristovski@qnx.com
    Initial QNX port of gdb-7.5.

--- tools/gdb/trunk/gdb-7.5/gdb/corelow.c
+++ tools/gdb/trunk/gdb-7.5/gdb/corelow.c
@@ -264,7 +264,11 @@ add_to_thread_list (bfd *abfd, asection *asect, void *reg_sect_arg)
       inf->fake_pid_p = fake_pid_p;
     }
 
+#ifndef __QNXTARGET__
   ptid = ptid_build (pid, lwpid, 0);
+#else /* __QNXTARGET__ */
+  ptid = ptid_build (pid, 0, lwpid);
+#endif /* __QNXTARGET__ */
 
   add_thread (ptid);
 
@@ -524,9 +528,15 @@ get_core_register_section (struct regcache *regcache,
 
   xfree (section_name);
 
+#ifdef __QNXTARGET__
+  if (ptid_get_tid (inferior_ptid))
+    section_name = xstrprintf ("%s/%ld", name,
+			       ptid_get_tid (inferior_ptid));
+#else /* ! __QNXTARGET__ */
   if (ptid_get_lwp (inferior_ptid))
     section_name = xstrprintf ("%s/%ld", name,
 			       ptid_get_lwp (inferior_ptid));
+#endif
   else
     section_name = xstrdup (name);
 
@@ -935,7 +945,7 @@ init_core_ops (void)
 }
 
 void
-_initialize_corelow (void)
+foo_initialize_corelow (void)
 {
   init_core_ops ();
 
--- tools/gdb/trunk/gdb-7.5/gdb/testsuite/gdb.base/fileio.c
+++ tools/gdb/trunk/gdb-7.5/gdb/testsuite/gdb.base/fileio.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/errno.h>
+#include <errno.h>
 #include <sys/types.h>
-#include <sys/fcntl.h>
+#include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <errno.h>
