49887dd03baaba81519609772e8108adf3701dfd
rmansfield@qnx.com <rmansfield@qnx.com@402436cc-5737-0410-82d2-8310154eca3c>
2015-03-04 15:20:03 +0000

    JI: 886679
    Fix -nostdinc++.

--- tools/gcc/trunk/gcc/config/nto.h
+++ tools/gcc/trunk/gcc/config/nto.h
@@ -43,15 +43,16 @@ do { \
 
 #define QNX_SYSTEM_INCLUDES \
 "%{!nostdinc: \
- %{stdlib=libcpp|stdlib=libcpp-ne: \
-  -isystem %$QNX_TARGET/usr/include/cpp/c/ \
-  -isystem %$QNX_TARGET/usr/include/cpp \
- } \
- %{!stdlib=libcpp: \
 -isystem %$QNX_HOST/usr/lib/gcc/" DEFAULT_TARGET_MACHINE "/%v1.%v2.%v3/include \
--isystem %$QNX_TARGET/usr/include/c++/%v1.%v2.%v3 \
--isystem %$QNX_TARGET/usr/include/c++/%v1.%v2.%v3/" DEFAULT_TARGET_MACHINE " \
--isystem %$QNX_TARGET/usr/include/c++/%v1.%v2.%v3/backward } \
+%{!nostdinc++: \
+%{stdlib=libcpp|stdlib=libcpp-ne: \
+ -isystem %$QNX_TARGET/usr/include/cpp/c/ \
+ -isystem %$QNX_TARGET/usr/include/cpp \
+} \
+%{!stdlib=libcpp: \
+ -isystem %$QNX_TARGET/usr/include/c++/%v1.%v2.%v3 \
+ -isystem %$QNX_TARGET/usr/include/c++/%v1.%v2.%v3/" DEFAULT_TARGET_MACHINE " \
+ -isystem %$QNX_TARGET/usr/include/c++/%v1.%v2.%v3/backward } } \
 -isysroot %$QNX_TARGET/}"
 
 /* Don't assume anything about the header files.  */
