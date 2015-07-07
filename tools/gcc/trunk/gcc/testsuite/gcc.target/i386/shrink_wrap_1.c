fcb165dd4fc3f3897ce89c25f98be8c0689c96f1
rmansfield@qnx.com <rmansfield@qnx.com@402436cc-5737-0410-82d2-8310154eca3c>
2015-07-07 15:01:55 +0000

    JI: 937757
    Add -fno-stack-protector to testcases that require it.

--- tools/gcc/trunk/gcc/testsuite/gcc.target/i386/shrink_wrap_1.c
+++ tools/gcc/trunk/gcc/testsuite/gcc.target/i386/shrink_wrap_1.c
@@ -1,5 +1,5 @@
 /* { dg-do compile { target { ! ia32 } } } */
-/* { dg-options "-O2 -fdump-rtl-pro_and_epilogue" } */
+/* { dg-options "-O2 -fno-stack-protector -fdump-rtl-pro_and_epilogue" } */
 
 enum machine_mode
 {
