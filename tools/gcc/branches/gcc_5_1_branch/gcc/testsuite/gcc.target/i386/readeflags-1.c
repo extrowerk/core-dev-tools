fdc2b046ecc53e50c7836c439ed8b7f2f6948daf
rmansfield <rmansfield@qnx.com>
2015-07-29 16:12:49 +0000

    JI: 937757
    Branch gcc 5.1.

---
+++ tools/gcc/branches/gcc_5_1_branch/gcc/testsuite/gcc.target/i386/readeflags-1.c
@@ -0,0 +1,41 @@
+/* { dg-do run } */
+/* { dg-options "-O0" } */
+/* { dg-options "-O0 -march=i686" { target { { i?86-*-* x86_64-*-* } && ia32 } } } */
+
+#include <x86intrin.h>
+
+#ifdef __x86_64__
+#define EFLAGS_TYPE unsigned long long int
+#else
+#define EFLAGS_TYPE unsigned int
+#endif
+
+static EFLAGS_TYPE
+readeflags_test (unsigned int a, unsigned int b)
+{
+  unsigned x = (a == b);
+  return __readeflags ();
+}
+
+int
+main ()
+{
+  EFLAGS_TYPE flags;
+
+  flags = readeflags_test (100, 100);
+
+  if ((flags & 1) != 0)  /* Read CF */
+    abort ();
+
+  flags = readeflags_test (100, 101);
+
+  if ((flags & 1) == 0)  /* Read CF */
+    abort ();
+
+#ifdef DEBUG
+    printf ("PASSED\n");
+#endif
+
+  return 0;
+}
+
