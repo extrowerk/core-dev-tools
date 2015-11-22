eec3a85c4b2084cc9e26ed08a93dce768583e5fd
rmansfield <rmansfield@qnx.com>
2015-11-22 21:26:11 +0000

    Branch binutils 2.25 targeting QNX 6.5.

---
+++ tools/binutils/branches/binutils_2_25_qnx650_win64/ld/testsuite/ld-arm/farcall-arm-arm.s
@@ -0,0 +1,21 @@
+@ Test to ensure that a ARM to ARM call exceeding 32Mb generates a stub.
+
+	.global _start
+	.type   _start, %function
+	.syntax unified
+
+@ We will place the section .text at 0x1000.
+
+	.text
+
+_start:
+	bl bar
+
+@ We will place the section .foo at 0x2001020.
+
+	.section .foo, "xa"
+
+	.type bar, %function
+bar:
+	bx lr
+
