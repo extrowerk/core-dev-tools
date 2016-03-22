6c5b69d61a19bf5dd7b6bb3d445f7ae7c4ecb54d
mdaniels@qnx.com <mdaniels@qnx.com@402436cc-5737-0410-82d2-8310154eca3c>
2016-03-22 20:50:09 +0000

    Add -stdlib=libc++ and allow static linking for all options.

--- tools/gcc/trunk/gcc/c-family/c-opts.c
+++ tools/gcc/trunk/gcc/c-family/c-opts.c
@@ -591,6 +591,7 @@ c_common_handle_option (size_t scode, const char *arg, int value,
 
     case OPT_stdlib_libcpp:
     case OPT_stdlib_libcpp_ne:
+    case OPT_stdlib_libc__:
     case OPT_nostdinc__:
       std_cxx_inc = false;
       break;
