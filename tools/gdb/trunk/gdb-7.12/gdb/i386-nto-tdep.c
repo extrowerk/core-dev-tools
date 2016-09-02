4f598232d6641560b4acd5119d7c81ad158baf67
mdaniels <mdaniels@qnx.com>
2016-09-02 18:21:16 +0000

    JI:1551687
    Keep my bad merge steak going and move on..
    
        Introduce nto_gdb_signal_from_target
        
        gdb/ChangeLog:
        
        	* i386-nto-tdep.c (aarch64_nto_init_abi): Install
        	 nto_gdb_signal_from_target.
        	* nto-tdep.c (nto_gdb_signal_from_target): New function.
        	* nto-tdep.h (nto_gdb_signal_from_target): New declaration.

--- tools/gdb/trunk/gdb-7.12/gdb/i386-nto-tdep.c
+++ tools/gdb/trunk/gdb-7.12/gdb/i386-nto-tdep.c
@@ -363,7 +363,8 @@ i386nto_init_abi (struct gdbarch_info info, struct gdbarch *gdbarch)
     }
   set_solib_ops (gdbarch, &nto_svr4_so_ops);
 
-  set_gdbarch_get_siginfo_type (gdbarch, nto_get_siginfo_type);
+  set_gdbarch_gdb_signal_to_target (gdbarch, nto_gdb_signal_to_target);
+  set_gdbarch_gdb_signal_from_target (gdbarch, nto_gdb_signal_from_target);
 }
 
 /* Provide a prototype to silence -Wmissing-prototypes.  */
