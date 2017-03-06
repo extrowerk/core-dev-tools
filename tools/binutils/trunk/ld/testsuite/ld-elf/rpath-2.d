8b6eeb77a53d6b06795a9bdd3a3d8def35572438
mdaniels@qnx.com <mdaniels@qnx.com@402436cc-5737-0410-82d2-8310154eca3c>
2017-03-06 15:53:13 +0000

    Stop using trunk for release..
    ..use explicit branches instead

--- tools/binutils/trunk/ld/testsuite/ld-elf/rpath-2.d
+++
@@ -1,8 +0,0 @@
-#source: start.s
-#readelf: -d -W
-#ld: -shared -rpath .
-#target: *-*-linux* *-*-gnu* *-*-nto*
-
-#...
- +0x[0-9a-f]+ +\(RPATH\) +Library rpath: +\[.\]
-#pass
--- tools/binutils/trunk/ld/testsuite/ld-elf/unknown2.d
+++
@@ -1,8 +0,0 @@
-#source: unknown2.s
-#ld: -shared
-#readelf: -S
-#target: *-*-linux* *-*-gnu* *-*-nto*
-
-#...
-  \[[ 0-9]+\] \.note.foo[ \t]+NOTE[ \t]+.*
-#pass
