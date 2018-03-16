a973849849e8fa3d3001bacb82cccfe4b3f6ac0f
bweber <bweber@qnx.com>
2018-03-16 14:15:49 +0000

    fix logic twist in default_speculation_safe_load() broaden testcase

--- tools/gcc/branches/700_release_retpoline/gcc/testsuite/gcc.dg/builtin-speculation-safe-load.c
+++ tools/gcc/branches/700_release_retpoline/gcc/testsuite/gcc.dg/builtin-speculation-safe-load.c
@@ -7,11 +7,17 @@ int main( int argc, char **argv ) {
 	unsigned char element=__builtin_speculation_safe_load( field+4, field, field+9 );
 /* { dg-warning "this target does not support anti-speculation operations." "No speculation barriers" { target { { ! arm*-*-*  } && { ! aarch64*-*-* } } } 7 } */	
 	if( element != 'e' ) abort();
-	element=__builtin_speculation_safe_load( field+9, field, field+9 );
+	element=__builtin_speculation_safe_load( field+4, (void *)0, field+9 );
 /* { dg-warning "this target does not support anti-speculation operations." "No speculation barriers" { target { { ! arm*-*-*  } && { ! aarch64*-*-* } } } 10 } */	
+	if( element != 'e' ) abort();
+	element=__builtin_speculation_safe_load( field+9, field, field+9 );
+/* { dg-warning "this target does not support anti-speculation operations." "No speculation barriers" { target { { ! arm*-*-*  } && { ! aarch64*-*-* } } } 13 } */	
+	if( element == 'j' ) abort();
+	element=__builtin_speculation_safe_load( field+9, (void *)0, field+9 );
+/* { dg-warning "this target does not support anti-speculation operations." "No speculation barriers" { target { { ! arm*-*-*  } && { ! aarch64*-*-* } } } 16 } */	
 	if( element == 'j' ) abort();
 	return 0;
 }
-/* { dg-final { scan-assembler "\thint\t#0x14\t// CSDB" { target aarch64*-*-* } } } */
-/* { dg-final { scan-assembler "\t.inst 0xf3af8014\t@ CSDB" { target arm*-*-* } } } */
+/* { dg-final { scan-assembler-times "\thint\t#0x14\t// CSDB" 4 { target aarch64*-*-* } } } */
+/* { dg-final { scan-assembler-times "\t.inst 0xf3af8014\t@ CSDB" 4 { target arm*-*-* } } } */
 
