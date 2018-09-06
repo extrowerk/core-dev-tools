b31560d40664e3d5548142de95fbca36f5cd0168
bweber@qnx.com <bweber@qnx.com@402436cc-5737-0410-82d2-8310154eca3c>
2018-09-06 09:46:49 +0000

    create experimental GCC 8.2 branch for testing JI:2517079

---
+++ tools/gcc/branches/gcc_8_2_branch/gcc/testsuite/gcc.dg/torture/pr68264.c
@@ -0,0 +1,151 @@
+/* { dg-do run } */
+/* { dg-add-options ieee } */
+/* { dg-require-effective-target fenv_exceptions } */
+
+#include <fenv.h>
+#include <math.h>
+#include <errno.h>
+
+extern void abort (void) __attribute__ ((noreturn));
+
+#define LARGE_NEG_MAYBE_ERANGE 0x01
+#define LARGE_NEG_ERANGE       0x02
+#define LARGE_POS_ERANGE       0x04
+#define LARGE_NEG_EDOM         0x08
+#define LARGE_POS_EDOM         0x10
+
+#define LARGE_ERANGE (LARGE_NEG_ERANGE | LARGE_POS_ERANGE)
+#define LARGE_EDOM (LARGE_NEG_EDOM | LARGE_POS_EDOM)
+#define POWER_ERANGE (LARGE_NEG_MAYBE_ERANGE | LARGE_POS_ERANGE)
+
+#define TEST(CALL, FLAGS) (CALL, tester (FLAGS))
+
+volatile double d;
+volatile int i;
+
+static void (*tester) (int);
+
+void
+check_quiet_nan (int flags __attribute__ ((unused)))
+{
+  if (fetestexcept (FE_ALL_EXCEPT))
+    abort ();
+  if (errno)
+    abort ();
+}
+
+void
+check_large_neg (int flags)
+{
+  if (flags & LARGE_NEG_MAYBE_ERANGE)
+    return;
+#if defined(__QNXNTO__)
+  int expected_fe=(flags & (LARGE_NEG_ERANGE|LARGE_NEG_EDOM) ) ? FE_ALL_EXCEPT
+			: 0;
+  if (!fetestexcept (expected_fe) & expected_fe)
+    abort ();
+  feclearexcept (FE_ALL_EXCEPT);
+#else
+  int expected_errno = (flags & LARGE_NEG_ERANGE ? ERANGE
+			: flags & LARGE_NEG_EDOM ? EDOM
+			: 0);
+  if (expected_errno != errno)
+    abort ();
+  errno = 0;
+#endif
+}
+
+void
+check_large_pos (int flags)
+{
+#if defined(__QNXNTO__)
+  int expected_fe=(flags & (LARGE_POS_ERANGE|LARGE_POS_EDOM) ) ? FE_ALL_EXCEPT
+			: 0;
+  if (!fetestexcept (expected_fe) & expected_fe)
+    abort ();
+  feclearexcept (FE_ALL_EXCEPT);
+#else
+
+  int expected_errno = (flags & LARGE_POS_ERANGE ? ERANGE
+			: flags & LARGE_POS_EDOM ? EDOM
+			: 0);
+  if (expected_errno != errno)
+    abort ();
+  errno = 0;
+#endif
+}
+
+void
+test (void)
+{
+  TEST (acos (d), LARGE_EDOM);
+  TEST (asin (d), LARGE_EDOM);
+  TEST (acosh (d), LARGE_NEG_EDOM);
+  TEST (atanh (d), LARGE_EDOM);
+  TEST (cosh (d), LARGE_ERANGE);
+  TEST (sinh (d), LARGE_ERANGE);
+  TEST (log (d), LARGE_NEG_EDOM);
+#if defined (__sun__) && defined (__unix__)
+  /* Disabled due to a bug in Solaris libm.  */
+  if (0)
+#endif
+    TEST (log2 (d), LARGE_NEG_EDOM);
+  TEST (log10 (d), LARGE_NEG_EDOM);
+#if defined(__GLIBC__) && (__GLIBC__ < 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ < 22))
+  /* Disabled due to glibc PR 6792, fixed in glibc 2.22.  */
+  if (0)
+#endif
+    TEST (log1p (d), LARGE_NEG_EDOM);
+  TEST (exp (d), POWER_ERANGE);
+#if (defined (__sun__) || defined(__hppa__)) && defined (__unix__)
+  /* Disabled due to a bug in Solaris libm.  HP PA-RISC libm doesn't support
+     ERANGE for exp2.  */
+  if (0)
+#endif
+    {
+      TEST (exp2 (d), POWER_ERANGE);
+#if defined(__GLIBC__) && (__GLIBC__ < 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ < 11))
+      /* Disabled due to glibc PR 6788, fixed in glibc 2.11.  */
+      if (0)
+#endif
+	TEST (expm1 (d), POWER_ERANGE);
+    }
+  TEST (sqrt (d), LARGE_NEG_EDOM);
+  TEST (pow (100.0, d), POWER_ERANGE);
+  TEST (pow (i, d), POWER_ERANGE);
+}
+
+int
+main (void)
+{
+  errno = 0;
+  i = 100;
+  d = __builtin_nan ("");
+  tester = check_quiet_nan;
+  feclearexcept (FE_ALL_EXCEPT);
+  test ();
+
+#if defined(__QNXNTO__)
+  feclearexcept (FE_ALL_EXCEPT);
+#endif
+
+  d = -1.0e80;
+  tester = check_large_neg;
+#if defined(__QNXNTO__)
+  feclearexcept (FE_ALL_EXCEPT);
+#else
+  errno = 0;
+#endif
+  test ();
+
+  d = 1.0e80;
+  tester = check_large_pos;
+#if defined(__QNXNTO__)
+  feclearexcept (FE_ALL_EXCEPT);
+#else
+  errno = 0;
+#endif
+  test ();
+
+  return 0;
+}
