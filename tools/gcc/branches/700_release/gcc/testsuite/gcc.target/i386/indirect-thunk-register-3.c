58f8f6d2e29f59f8e8dad2ff88e382516b7b17a2
bweber <bweber@qnx.com>
2018-11-02 09:36:00 +0000

    Fix retpoline dejagnu testcases with regards to pic/pie JI:2567428 CR:amallory@qnx.com https://bts-reviewboard.bts.rim.net/r/216506/

--- tools/gcc/branches/700_release/gcc/testsuite/gcc.target/i386/indirect-thunk-register-3.c
+++ tools/gcc/branches/700_release/gcc/testsuite/gcc.target/i386/indirect-thunk-register-3.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -mindirect-branch=thunk-extern -mindirect-branch-register -fno-pic" } */
+/* { dg-options "-O2 -mindirect-branch=thunk-extern -mindirect-branch-register -fno-pie" } */
 
 typedef void (*dispatch_t)(long offset);
 
