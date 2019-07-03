14ed41c05c5f4c7c08d5072cfb8792e0d9161909
bweber@qnx.com <bweber@qnx.com@402436cc-5737-0410-82d2-8310154eca3c>
2019-07-03 22:19:59 +0000

    Create and populate gcc_8_3_branch repository JI:2517079 CI:cpavel@qnx.com CI:jsleeth@qnx.com https://bts-reviewboard.bts.rim.net/r/229611/

---
+++ tools/gcc/branches/gcc_8_3_branch/stage/host/linux/x86_64/usr/lib/gcc/arm-unknown-nto-qnx7.1.0eabi/8.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --srcdir=.. --enable-gnu-indirect-function=yes --enable-cxx-flags=-stdlib=libstdc++ --enable-cheaders=c_global --enable-initfini-array --enable-default-pie --with-as=arm-unknown-nto-qnx7.1.0eabi-as --with-ld=arm-unknown-nto-qnx7.1.0eabi-ld --with-float=hard --with-arch=armv7-a --with-fpu=vfpv3 --with-mode=thumb --prefix=/opt/qnx710/host/linux/x86_64/usr --libexecdir=/opt/qnx710/host/linux/x86_64/usr/lib --with-gxx-include-dir=/opt/qnx710/target/qnx7/usr/include/c++/8.3.0 --enable-threads=posix --enable-__cxa_atexit --enable-languages=c++ --with-sysroot=/home/bweber/qnxmainline/target/qnx7 --target=arm-unknown-nto-qnx7.1.0eabi --disable-nls --disable-tls --disable-libssp --disable-libstdcxx-pch --enable-libgomp --enable-shared --enable-multilib --with-bugurl=http://www.qnx.com --enable-default-ssp CC=x86_64-unknown-linux-gnu-gcc CFLAGS='-O2 -m64' LDFLAGS='-Wl,-s ' CXX=x86_64-unknown-linux-gnu-g++ CXXFLAGS='-O2 -m64' AUTOMAKE=: AUTOCONF=: AUTOHEADER=: AUTORECONF=: ACLOCAL=:";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm6" }, { "arch", "armv7-a" }, { "float", "hard" }, { "mode", "thumb" }, { "fpu", "vfpv3" } };
