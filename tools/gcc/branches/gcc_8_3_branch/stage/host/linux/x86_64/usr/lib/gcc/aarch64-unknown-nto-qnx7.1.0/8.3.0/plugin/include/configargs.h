ecd6828e6207f5bcda93de35cc29036f3ff9a2da
bweber@qnx.com <bweber@qnx.com@402436cc-5737-0410-82d2-8310154eca3c>
2019-07-08 13:19:56 +0000

    remove stage directory JI:2517079 CR:thaupt@qnx.com CR:cpavel@qnx.com https://bts-reviewboard.bts.rim.net/r/230364/

--- tools/gcc/branches/gcc_8_3_branch/stage/host/linux/x86_64/usr/lib/gcc/aarch64-unknown-nto-qnx7.1.0/8.3.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../configure --srcdir=.. --enable-gnu-indirect-function=yes --enable-cxx-flags=-stdlib=libstdc++ --enable-cheaders=c_global --enable-initfini-array --enable-default-pie --with-as=aarch64-unknown-nto-qnx7.1.0-as --with-ld=aarch64-unknown-nto-qnx7.1.0-ld --prefix=/opt/qnx710/host/linux/x86_64/usr --libexecdir=/opt/qnx710/host/linux/x86_64/usr/lib --with-gxx-include-dir=/opt/qnx710/target/qnx7/usr/include/c++/8.3.0 --enable-threads=posix --enable-__cxa_atexit --enable-languages=c++ --with-sysroot=/home/bweber/qnxmainline/target/qnx7 --target=aarch64-unknown-nto-qnx7.1.0 --disable-nls --disable-tls --disable-libssp --disable-libstdcxx-pch --enable-libgomp --enable-shared --enable-multilib --with-bugurl=http://www.qnx.com --enable-default-ssp CC=x86_64-unknown-linux-gnu-gcc CFLAGS='-O2 -m64' LDFLAGS='-Wl,-s ' CXX=x86_64-unknown-linux-gnu-g++ CXXFLAGS='-O2 -m64' AUTOMAKE=: AUTOCONF=: AUTOHEADER=: AUTORECONF=: ACLOCAL=:";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { NULL, NULL} };
