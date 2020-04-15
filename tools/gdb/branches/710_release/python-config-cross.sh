f18ed1d5629428bc12353ed6d7c58b8aa0c81cc1
bweber@qnx.com <bweber@qnx.com@402436cc-5737-0410-82d2-8310154eca3c>
2020-04-15 10:36:21 +0000

    [710] GDB - enable mingw build against Python3 JI: 2870954 CI: cpavel@qnx.com CI: thaupt@qnx.com CR: https://bts-reviewboard.bts.rim.net/r/243369/
    while trying to build GDB against Python3 the win64/mingw build failed in the python part. To fix this the proper upstream commit was applied to the GDB source. This patch will not change behaviour with Python2 builds. Also the changes in the python-config-cross.sh script only affect experimental desktop builds which usually do not include any Python support at all. The Jenkins builds use their own version of the config script.

--- tools/gdb/branches/710_release/python-config-cross.sh
+++ tools/gdb/branches/710_release/python-config-cross.sh
@@ -1,70 +1,56 @@
 #!/bin/bash
 
-# Script for cross-compiling win32 and darwin on linux host.
+# Script for cross-compiling win64 and darwin and linux on linux host.
 # Provided to gdb configure as an argument to --with-python
 #
-# It needs to respond to --prefix --exec-prefix --includes --libs --cflags --ldflags arguments.
+# It needs to respond to --exec-prefix --includes and --ldflags arguments.
 # Gdb configure will pass python-configure.py (path to it) followed by one of
 # the above options.
+#
+# also the build script or build-hooks must make sure that ${OS} and
+# ${PREREQ_DIR} are set and exported
 
+echo "$*" >> python-config-cross.log
+if [ "${PREREQ_DIR}" == "" ]; then
+    PREREQ_DIR=$(realpath ${PWD}/../../../GDBPython)
+fi
+
+# echo "OS=${TARGET_SYSNAME}, PREREQ_DIR=${PREREQ_DIR}" >> python-config-cross.log
+python_root=${PREREQ_DIR}/${TARGET_SYSNAME}
+py_include=-I${python_root}/usr/include/python3.8
+py_exec_prefix="${prefix}/bin"
+py_libs="-L${python_root}/usr/lib -lpython3.8"
 
 case ${TARGET_SYSNAME} in
-win32*)
-	gdb_prereq_dir=${PWD}/../../../gdb-prereq/win32
-	python_root=${gdb_prereq_dir}/Python27
-	python_ver=2.7
-	py_include="-I${python_root}/include -I${python_root}/include/python${python_ver} -DWINVER=0x0500 "
-	py_libs="-L${python_root}/libs  -lpython27"
-	# py_exec_prefix sets up WITH_PYTHON_PATH in config.h
-	# This is needed to automatically setup PYTHONHOME env. var.
-	py_exec_prefix="${prefix}/bin"
+win64)
+	py_include="-I${python_root}/usr/include ${py_include} -DWINVER=0x0500"
+	py_libs="-L${python_root}/usr/lib -lpython3.8.dll"
 	;;
-win64*)
-	gdb_prereq_dir=${PWD}/../../../gdb-prereq/win64
-	python_root=${gdb_prereq_dir}/Python27
-	python_ver=2.7
-	py_include="-I${python_root}/include -I${python_root}/include/python${python_ver} -DWINVER=0x0500 "
-	py_libs="-L${python_root}/libs  -lpython27"
-	# py_exec_prefix sets up WITH_PYTHON_PATH in config.h
-	# This is needed to automatically setup PYTHONHOME env. var.
-	py_exec_prefix="${prefix}/bin"
+darwin)
 	;;
-darwin*)
-	gdb_prereq_dir=${PWD}/../../../gdb-prereq/darwin
-	python_root=${gdb_prereq_dir}/Python27
-	python_ver=2.7
-	py_include="-I${python_root}/include -I${python_root}/include/python${python_ver}"
-	py_libs="-L${python_root}/lib -lpython${python_ver}"
-	# py_exec_prefix sets up WITH_PYTHON_PATH in config.h
-	# This is needed to automatically setup PYTHONHOME env. var.
-	py_exec_prefix="${prefix}"
-	;;
-linux*)
-	gdb_prereq_dir=${PWD}/../../../gdb-prereq/linux/${PYTHON_CPU}
+linux)
 	# For testing, it is convenient to be able to run gdb from build directory
-	ln -fs ${gdb_prereq_dir}/python27 ..
-	python_root=${gdb_prereq_dir}/python27
-	python_ver=2.7
-	py_include="-I${python_root}/include -I${python_root}/include/python${python_ver}"
-	py_libs="-L${python_root}/lib -lpython${python_ver}"
-	# py_exec_prefix sets up WITH_PYTHON_PATH in config.h
-	# This is needed to automatically setup PYTHONHOME env. var.
-	py_exec_prefix="${prefix}"
+#	ln -fs ${gdb_prereq_dir}/python27 ..
 	;;
 *)
-	echo "Not configured for python"
+	echo "${TARGET_SYSNAME} is not configured for python" >> python-config-cross.log
 	exit 1
 	;;
 esac
 
 
 if [ $2 == --includes ] ; then
-  echo "${py_include}" 
+  echo "${py_include}"
+  echo "${py_include}" >> python-config-cross.log
 elif [ $2 == --ldflags ] ; then
-  echo "${py_libs}" 
+  echo "${py_libs}"
+  echo "${py_libs}" >> python-config-cross.log
 elif [ $2 == --exec-prefix ] ; then
   echo "${py_exec_prefix}"
+  echo "${py_exec_prefix}" >> python-config-cross.log
 else
+echo "Unknown parameter $2" >> python-config-cross.log
   exit 1
 fi
+echo "" >> python-config-cross.log
 
