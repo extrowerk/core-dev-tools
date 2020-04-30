ecaec22b89606c647b1d75aaed6117648335e851
bweber@qnx.com <bweber@qnx.com@402436cc-5737-0410-82d2-8310154eca3c>
2020-04-30 13:55:38 +0000

    Re-introduce .qnx_link_map to BFD JI: 2875610 CI: thaupt@qnx.com CI: swebb@blackberry.com CR: https://bts-reviewboard.bts.rim.net/r/244133/

--- tools/binutils/branches/binutils-2.32/bfd/elf.c
+++ tools/binutils/branches/binutils-2.32/bfd/elf.c
@@ -10710,6 +10710,8 @@ elfcore_grok_nto_regs (bfd *abfd,
 #define BFD_QNT_CORE_STATUS	8
 #define BFD_QNT_CORE_GREG	9
 #define BFD_QNT_CORE_FPREG	10
+#define BFD_QNT_LINK_MAP	11
+#define BFD_QNT_LINK_MAP_SEC_NAME ".qnx_link_map"
 
 static bfd_boolean
 elfcore_grok_nto_note (bfd *abfd, Elf_Internal_Note *note)
@@ -10729,6 +10731,9 @@ elfcore_grok_nto_note (bfd *abfd, Elf_Internal_Note *note)
       return elfcore_grok_nto_regs (abfd, note, tid, ".reg");
     case BFD_QNT_CORE_FPREG:
       return elfcore_grok_nto_regs (abfd, note, tid, ".reg2");
+    case BFD_QNT_LINK_MAP:
+      return elfcore_make_note_pseudosection (abfd, BFD_QNT_LINK_MAP_SEC_NAME,
+                                              note);
     default:
       return TRUE;
     }
